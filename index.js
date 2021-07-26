cc7b8eae728fbb2c100be97910c3ccb7a55d5453
Ohrannick <ohrannick@gmail.com>
2021-07-26 22:37:40 +0300

    little change

--- index.js
+++ index.js
@@ -1,43 +1,53 @@
 const colors = require('colors')
 // console.log(colors.red(`Hello world, ${process.argv[2]}`));
+let numBegin = process.argv[2];
+let numEnd = process.argv[3];
 
 //Задание. Простые числа
-  function primeNumber (n){
-    let i=2, j=0;
-    if (n <= 1)
-      return "";
-    else {
-      for (i; i<=n; i++){
-        if (n % i == 0) {
-          j++;
-        }
+function primeNumber (n){
+  let i=2, j=0;
+  if (n <= 1)
+    return "";
+  else {
+    for (i; i<=n; i++){
+      if (n % i == 0) {
+        j++;
       }
-      if (j==1){
-        return n;
-      }
-      return "";
     }
+    if (j==1){
+      return n;
+    }
+    return "";
   }
+}
 
-  var mas = [], count = 0;
-  while (count <= process.argv[2]){
-    if (!primeNumber(count)=="")
-      mas.push(primeNumber(count));
-    count++;
+const mas = [];
+if (isNaN(numBegin) || isNaN(numEnd)){
+  console.log(colors.red('Введите числа'));
+} else {
+  while (numBegin <= numEnd){
+    if (!primeNumber(numBegin)=="")
+      mas.push(primeNumber(numBegin));
+    numBegin++;
   }
+}
 
-let i=0;
-while (i<mas.length){
-  console.log(colors.red(mas[i]));
-  i++
-  if(i >= mas.length){
-    return;
-  };
-  console.log(colors.yellow(mas[i]));
-  i++;
-  if(i >= mas.length){
-    return;
-  };
-  console.log(colors.green(mas[i]));
-  i++;
+let x=0;
+if (mas.length < 1){
+  console.log(colors.green('Простых чисел нет'));
+} else {
+  while (x < mas.length){
+    console.log(colors.red(mas[x]));
+    x++
+    if(x >= mas.length){
+      return;
+    };
+    console.log(colors.yellow(mas[x]));
+    x++;
+    if(x >= mas.length){
+      return;
+    };
+    console.log(colors.green(mas[x]));
+    x++;
+  }
 }
